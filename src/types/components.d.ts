39541e611082730e1bb73ceda21eb870aaac72bf
JackeyChen007 <150228205+JackeyChen007@users.noreply.github.com>
2024-03-06 11:38:39 +0800

    更新 components.d.ts

--- src/types/components.d.ts
+++ src/types/components.d.ts
@@ -7,20 +7,12 @@ export {}
 
 declare module 'vue' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./../components/HelloWorld.vue')['default']
     Hotel: typeof import('./../components/hotel/index.vue')['default']
-    IconCommunity: typeof import('./../components/icons/IconCommunity.vue')['default']
-    IconDocumentation: typeof import('./../components/icons/IconDocumentation.vue')['default']
-    IconEcosystem: typeof import('./../components/icons/IconEcosystem.vue')['default']
-    IconSupport: typeof import('./../components/icons/IconSupport.vue')['default']
-    IconTooling: typeof import('./../components/icons/IconTooling.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
     SvgIcon: typeof import('./../components/svgIcon/index.vue')['default']
-    TheWelcome: typeof import('./../components/TheWelcome.vue')['default']
     VanButton: typeof import('vant/es')['Button']
     VanTabbar: typeof import('vant/es')['Tabbar']
     VanTabbarItem: typeof import('vant/es')['TabbarItem']
-    WelcomeItem: typeof import('./../components/WelcomeItem.vue')['default']
   }
 }
